a1a1f57f939e874c4d4231891ef9c58ba35be598
alex furtado <furtado.vm2@gmail.com>
2025-04-29 22:25:24 -0300

    Fix: use $PORT for Render compatibility

--- start-django.sh
+++ start-django.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 export GUNICORN_WORKERS=2 
+
 python manage.py collectstatic --no-input
+
 # Apply migrations
 python manage.py migrate
-if [[ "$ENV_STATE" == "production" ]]; then
-    gunicorn --bind 0.0.0.0:8000 core.wsgi --workers ${GUNICORN_WORKERS:-2} --forwarded-allow-ips "*"
+
+if [ "$ENV_STATE" = "production" ]; then
+    gunicorn --bind 0.0.0.0:$PORT core.wsgi --workers ${GUNICORN_WORKERS:-2} --forwarded-allow-ips "*"
 else
     python manage.py runserver 0.0.0.0:8000
 fi
